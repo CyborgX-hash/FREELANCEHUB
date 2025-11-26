b1f69d1f1336ecd7c202c3055361a2182a0d8f9f
CyborgX-hash <sakshamsontakke713@gmail.com>
2025-11-26 22:31:55 +0530

    new updated in track and env exmaple addded

--- backend/src/routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createProject, getProjectsByClient } from "../controllers/projectController.js";
-
-const router = express.Router();
-
-router.post("/create", createProject);
-router.get("/client/:clientId", getProjectsByClient);
-
-export default router;
