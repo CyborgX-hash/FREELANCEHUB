db29860127652865cd77189c243e91d1bb848b01
Sankalp <sankalp13353@gmail.com>
2026-04-10 20:21:40 +0530

    in routes proposal route updated

--- backend/src/routes/proposalRoute.js
+++ backend/src/routes/proposalRoute.js
@@ -2,20 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 const { authMiddleware } = require("../middlewares/userMiddleware");
-const proposalController = require("../controllers/proposalController");
 
-console.log("Loaded proposalController:", proposalController);
+// Repositories
+const ApplicationRepository = require("../repositories/ApplicationRepository");
+const ProjectRepository = require("../repositories/ProjectRepository");
 
-router.post(
-  "/",
-  authMiddleware,
-  proposalController.createProposalController
+// Service
+const ProposalService = require("../services/ProposalService");
+
+// Controller
+const ProposalController = require("../controllers/proposalController");
+
+// Dependency Injection
+const applicationRepository = new ApplicationRepository();
+const projectRepository = new ProjectRepository();
+const proposalService = new ProposalService(
+  applicationRepository,
+  projectRepository
 );
+const proposalController = new ProposalController(proposalService);
+
+router.post("/", authMiddleware, proposalController.create);
 
 router.get(
   "/project/:projectId",
   authMiddleware,
-  proposalController.getProposalsByProjectController
+  proposalController.getByProject
 );
 
 module.exports = router;
